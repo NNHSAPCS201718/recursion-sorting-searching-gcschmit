0dfcb0c4c4a52ccbc7e9d728516f2443ec836ff0
Geoff Schmit <gcschmit@gmail.com>
2018-03-13 12:23:32 -0500

    implemented the StringReverse example

---
+++ ClassNotes/StringReverse.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class StringReverse here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StringReverse
+{
+    public static String reverse( String str )
+    {
+        // must have a terminating case
+        if( str.equals( "" ))
+        {
+            return str;
+        }
+        
+        // must make the problem simplier
+        String firstChar = str.substring( 0, 1 );
+        String restOfString = str.substring( 1 );
+        
+        // recurse - call this method recursively with a simplier problem
+        String restOfStringReversed = reverse( restOfString );
+        
+        // return the solution
+        String strReversed = restOfStringReversed + firstChar;
+        return strReversed;
+        
+    }
+}
